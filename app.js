3ef7e7d6ba36d5963b3925384e6b989c30fca606
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-14 17:45:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express")
-const loginController = require("./controllers/login")
-const registerController = require("./controllers/register")
+const express = require('express');
+const loginController = require('./controllers/login');
+const registerController = require('./controllers/register');
 
-const app = express()
-app.set("view engine", "ejs")
-app.use(express.static("style"));
-app.get("/", loginController.rootAccessControl)
-app.get("/register", registerController.rootAccessControl)
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('style'));
+app.get('/', loginController.rootAccessControl);
+app.get('/register', registerController.rootAccessControl);
 
-app.listen(3000, () =>{
-   console.log("Start express server by specifying port 3000.")
-})
+app.listen(3000, () => {
+  console.log('Start express server by specifying port 3000.');
+});
