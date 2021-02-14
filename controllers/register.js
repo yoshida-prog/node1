2f553dbd1c7288d9c56334360c53a9943f375d18
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-14 17:43:42 +0900

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,10 +1,20 @@
-exports.rootAccessControl = (req, res) =>{
-   console.log("Access root node.")
-   let param = {
-       title: "Laravel",
-       loginLink: {href: "/", text: "Login"},
-       registerLink: {href: "/register", text: "Register"},
-       form: {name: "Name", email: "E-mail Address", password: "Password", confirm: "Comfirm Password"}
-   }
-   res.render("register", param)
-}
+exports.rootAccessControl = (req, res) => {
+  const param = {
+    title: 'Laravel',
+    loginLink: {
+      href: '/',
+      text: 'Login'
+    },
+    registerLink: {
+      href: '/register',
+      text: 'Register'
+    },
+    form: {
+      name: 'Name',
+      email: 'E-mail Address',
+      password: 'Password',
+      confirm: 'Comfirm Password'
+    }
+  }
+  res.render('register', param);
+};
